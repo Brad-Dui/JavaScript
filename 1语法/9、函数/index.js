67f2d99240d7b2263d9a85684b76d11550e652e6
hcq_pm <hcq_pm@163.com>
2021-07-09 21:42:44 +0800

    bind绑定方法 改变this指向

--- 1语法/9、函数/index.js
+++ 1语法/9、函数/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-06-05 21:11:21
- * @LastEditTime: 2021-06-27 22:26:33
+ * @LastEditTime: 2021-07-09 21:41:35
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \语法\9、函数\index.js
@@ -21,6 +21,10 @@
  * 4.获取最终显示的样式
  * 兼容IE obj.currentStyle.attr
  * 兼容Chorm getComputedStyle(obj,*).attr 
+ * 
+ * 5. bind绑定方法 改变this指向 相较与call()与apply()要对一个()
+ * 创建一个新的函数，在 bind() 被调用时，这个新函数的 this 被指定为 bind() 的第一个参数，
+ * 而其余参数将作为新函数的参数，供调用时使用
  */
 window.onload = function () {
 
@@ -58,7 +62,7 @@ window.onload = function () {
     // console.log(box1.currentStyle.height);               //只在IE下兼容
     // console.log(getComputedStyle(box1, false).width);   //不完全兼容IE
     console.log(box1.currentStyle);                        //在Chrome下为undefined 在IE下为object
-    //解决兼容问题(可以封装成一个获取样式的方法)
+    //4、解决兼容问题(可以封装成一个获取样式的方法)
     if (box1.currentStyle) {
         //IE
         console.log(box1.currentStyle.width);
@@ -67,4 +71,30 @@ window.onload = function () {
         //chrome
         console.log(getComputedStyle(box1, false).width);
     }
+
+    //5 bind()方法
+    window.name = 'win';
+    var name = 'a';
+    function getName() {
+        console.log(this.name);
+    }
+    var a = {
+        name: 'b',
+        getName() {
+            console.log(this.name);
+            getName()
+        },
+        c: {
+            name: 'c',
+            getName() {
+                getName.bind(this)()//将getName绑定到this
+            }
+        }
+    }
+    var getName2 = a.c.getName;
+
+    getName();//该函数为全局函数 this指向window         打印win
+    a.getName();//第一个this指向a 后面的函数为全局函数   打印b、win
+    a.c.getName();//将getName绑定在c                   打印c
+    getName2();//将getName绑定在2 对象为window          打印win
 }
